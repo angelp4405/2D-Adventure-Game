fdf32ba11b72b5785720bdbda3a8cf4135a786e2
angel <avasquez27@aoiths.org>
2024-11-20 09:23:28 -0500

    Set up a basic health system

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,14 +6,22 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 
 {
+    // Variables related to player movement
     public InputAction MoveAction;
     Rigidbody2D rigidbody2D;
-    Vector2 move;                 
+    Vector2 move;
+    public float speed = 3.0f;
+
+    // Variables related to the health system
+    public int maxHealth = 5;
+    int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
      MoveAction.Enable();
      rigidbody2D = GetComponent<Rigidbody2D>();
+     currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -23,9 +31,16 @@ public class PlayerController : MonoBehaviour
         Debug.Log(move);
     }
 
+    //FixedUpdate has the same call rate as the physics system
     void FixedUpdate()
     {
        Vector2 position = (Vector2)transform.position + move * 6.0f * Time.deltaTime;
        rigidbody2D.MovePosition(position);
+    }
+
+    void ChangeHealth (int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
